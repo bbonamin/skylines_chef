e35ae33dff251e61d9c06fd70bd4c05b3a2ab63a
Bruno Bonamin <bruno@bonamin.org>
2013-01-24 14:44:11 -0300

    All commented except for package installs

--- cookbooks/skylines_main/recipes/default.rb
+++ cookbooks/skylines_main/recipes/default.rb
@@ -10,25 +10,25 @@
   package pkg
 end
 
-execute "setup python dependencies" do
-  cwd "/vagrant"
-  command "sudo python setup.py develop"
-end
+# execute "setup python dependencies" do
+#   cwd "/vagrant"
+#   command "sudo python setup.py develop"
+# end
 
-execute "setup database prerequisites" do
-  user "postgres"
-  command "createuser vagrant && createdb skylines --o vagrant && createlang plpgsql -d skylines"
-end
+# execute "setup database prerequisites" do
+#   user "postgres"
+#   command "createuser vagrant && createdb skylines --o vagrant && createlang plpgsql -d skylines"
+# end
 
-execute "setup postgis" do
-  user "postgres"
-  command "psql -d skylines -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql"
-end
+# execute "setup postgis" do
+#   user "postgres"
+#   command "psql -d skylines -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql"
+# end
 
-execute "setup spatial reference system" do
-  user "postgres"
-  command "psql -d skylines -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql"
-end
+# execute "setup spatial reference system" do
+#   user "postgres"
+#   command "psql -d skylines -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql"
+# end
 
 # PENDING
 # execute "setup psql grants" do
@@ -39,10 +39,10 @@ end
 #   # \q
 # end
 
-execute "tg.devtools installation" do
-  cwd "/vagrant"
-  command "sudo easy_install tg.devtools"
-end
+# execute "tg.devtools installation" do
+#   cwd "/vagrant"
+#   command "sudo easy_install tg.devtools"
+# end
 
 # execute "setup app development env" do
 #   cwd "/vagrant"
@@ -54,8 +54,8 @@ end
 #   command "python import_airspaces.py development.ini assets/airspace/airspace_list.txt assets/airspace/airspace_blacklist.txt"
 # end
 
-service "ufw" do
-  action :disable
-end
+# service "ufw" do
+#   action :disable
+# end
 
 # paster serve development.ini
